55f1f9ca80c83da3826e9ee1b4d177e31bea37e3
SAURABH JHA <81814933+saurabhjha06@users.noreply.github.com>
2022-02-13 11:26:20 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+/*var a=8;
+var b=12;
+var c=a+b;
+
+console.log("sum"+c);
+*/
+var os_info=require('os');
+console.log("os type" +os_info.type());
+console.log("os type" +os_info.version());
+console.log("os type"+os_info.hostname());
+
