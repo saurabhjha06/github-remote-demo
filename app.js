c1b0997c69ee9c2ec878dae19e89ad9d32469766
Saurabh <saurabh_jha2@persistent.com>
2022-02-13 12:10:45 +0530

    uptime method implemented

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 /*var a=8;
 //using node inbuilt modules
+
 var b=12;
 var c=a+b;
 
@@ -9,4 +10,7 @@ var os_info=require('os');
 console.log("os type" +os_info.type());
 console.log("os type" +os_info.version());
 console.log("os type"+os_info.hostname());
+
+//adding some more os methods
+console.log("os uptime"+os.uptime());
 
